ee0978c7eb9be2f4c6f05e225ffae0fa8fafdce0
aws09 <aws09@CESAEPRT.WAN>
2025-04-22 17:19:13 +0100

    Ficha03_for

--- Introducao_Programacao/FichaPratica_03/ex_04.py
+++ Introducao_Programacao/FichaPratica_03/ex_04.py
@@ -7,9 +7,9 @@ else:
     divisor = 0
 
     for i in range(1, numero + 1):
-        if numero % 1 == 0:
+        if numero % i == 0:
             divisor += 1
-            if divisor == 2:
-                    print(f"O número {numero} escolhido é primo.")
-            else:
-                 print("É primo.")
+    if divisor == 2:
+        print("O número é primo.")
+    else:
+        print("Não é primo.")
